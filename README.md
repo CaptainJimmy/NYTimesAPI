90ec777b3a82774ec43ff0756f815ae48cea1747
CaptainJimmy <captainjimscharters@gmail.com>
2017-09-01 23:53:51 -0400

    updated files with new code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #NY Times API Portal
 
-I wrote this to work with the NY Times API.  Content is generated with jquery calls to the DOM and AJAX calls to the NYtimes API.
+I wrote this to work with the NY Times API.  Content is generated with jquery calls to the DOM / Form elements, and AJAX calls to the NYtimes API.  Content is created in its own bootstrap containers as needed and prepended to the DOM elements. 
 
 Toolsets used: Bootstrap, Jquery, Ajax, JSON, Random Offsets, Javascript, CSS
--- assets/js/nytimes.js
+++ assets/js/nytimes.js
@@ -1,23 +1,89 @@
 $(document).ready(function(){
 
+var authKey="fb1351dc47cc4e2b9335f0c5ac186dee";
+var queryURL;
+var searchType;
 
 //build the ajax call
 $("#times-form").on("submit", function(event) {
       event.preventDefault();
-        // This line grabs the input from the textbox
-        var topic = $("#times-input").val().trim();
-        var selectionNumber = $("#articles-selection").val();
-        var startYear = $("start-year-input").val();
-        var endYear = $("end-year-input").val();
-    
-		ajaxCall();
+          	searchType="search";
+          	console.log(searchType);
+		search(searchType);
 	});
+
+
+$("#topStories").on("click", function(event){
+	 event.preventDefault();
+	searchType="topStories";
+	console.log(searchType);
+		search(searchType);
+});
+
+$("#mostPopular").on("click", function(event){
+	 event.preventDefault();
+	searchType="mostPopular";
+	console.log(searchType);
+		search(searchType);
+});
+
+$("#clear").on("click", function(event){
+	 event.preventDefault();
+	$("#selectionPanel").empty();
+});
 
 //Build the AJAX Call URL to the API with input, and execute it.
 //Convert the response to bootstrap wells, and append them to the DOM at selectionPanel
-function ajaxCall(){
+function search(searchType){
+	//execute the search function
+if (searchType === "search"){
 
+queryURL="https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+  authKey + "&q=" + topic;
+    startYear=startYear.replace(/-/g,'');
+    endYear=endYear.replace(/-/g,'');
+  // This line grabs the input from the textbox
+        var topic = $("#times-input").val().trim();
+        var selectionNumber = $("#articles-selection").val().trim();
+        var startYear = $("#start-year-input").val();
+        var endYear = $("#end-year-input").val();
+    console.log(topic);
+    console.log(selectionNumber);
+    console.log(startYear);
+    console.log(endYear);
 
+  $.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+		});
+
+}
+		//execute the topStories
+else if (searchType === "topStories"){ 
+queryURL="https://api.nytimes.com/svc/topstories/v2/home.json?api-key="+authKey;
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+		});
+}
+	//execute most popular	
+else if (searchType==="mostPopular"){
+ queryURL="https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key="+authKey;
+ $.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+
+	});
+}
+
+		//oops
+
+else{
+	console.log("ERROR AND STUFF");
+}
 
 }
 
